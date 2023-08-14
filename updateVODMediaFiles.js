53fb525ee9843a3223489ec33d58df976f1554a6
Braden Stevenson <braden.stevenson@forumcomm.com>
2023-08-14 13:55:01 -0500

    updated node js to be async

--- updateVODMediaFiles.js
+++ updateVODMediaFiles.js
@@ -1,39 +1,58 @@
 const sdk = require('api')('@jwp-platform/v1.0#5jjp925ulkvczvf9');
 
-const siteId = 'jWZqVjvO';
+const siteId = 'SITE_ID_HERE';
 
-sdk.auth('jpP_C6yJ3E3bW_qVlHmVxWInZFU1T1pVeEdTMU50VEhkM1QybEpUVEpJYURNeGVsbzAn');
+sdk.auth('ADMIN_API_KEY_HERE');
 sdk.getV2SitesSite_idMedia({
   page: '1',
-  page_length: '20',
+  page_length: '10',
   q: 'hosting_type%3A%20hosted',
-  sort: 'created%3Aasc',
+  sort: 'publish_start_date%3Aasc',
   site_id: siteId
 })
-.then(({ data }) => {
-  data.media.forEach(item => {
+.then(async ({ data }) => {
+  console.log('Total items:', data.media.length);
 
-    let updatedCustomParams = {
-        requires_authentication: 'true'
-      };
-    if (item.metadata.custom_params !== null || Object.keys(item.metadata.custom_params).length !== 0){
-      updatedCustomParams = {
-          ...item.metadata.custom_params,
+  for (const item of data.media) {
+    try {
+      let updatedCustomParams = {};
+
+      if (!item.metadata.custom_params || Object.keys(item.metadata.custom_params).length === 0) {
+        updatedCustomParams = {
           requires_authentication: 'true'
         };
-    }
-      sdk.patchV2SitesSite_idMediaMedia_id({
-        metadata: {
-          custom_params: updatedCustomParams
+      } else {
+        const params = item.metadata.custom_params;
+        updatedCustomParams = {
+          ...params,
+          requires_authentication: 'true'
+        };
+      }
 
+      console.log('Updating custom params for media_id:', item.id);
+      console.log('custom params are:', item.metadata.custom_params);
+      console.log('custom params after update are:', updatedCustomParams);
+
+      await sdk.patchV2SitesSite_idMediaMedia_id(
+        {
+          metadata: {
+            custom_params: updatedCustomParams
+          }
+        },
+        {
+          site_id: siteId,
+          media_id: item.id
         }
-      }, {
-        site_id: siteId,
-        media_id: item.id
-      })
+      );
 
-    console.log('Custom param setup:', updatedCustomParams);
-  });
+      console.log('Custom param updated for media_id:', item.id);
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.error(`Error updating custom params for media_id ${item.id}:`, error.response.data);
+      } else {
+        console.error(`Error updating custom params for media_id ${item.id}:`, error);
+      }
+    }
+  }
 })
 .catch(err => console.error(err));
-
